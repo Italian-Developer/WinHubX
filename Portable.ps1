56e8f34ff55af17e00b80a1103e485753320559d
MrNico98 <136261932+MrNico98@users.noreply.github.com>
2024-07-15 22:09:09 +0200

    Update Portable.ps1

--- Portable.ps1
+++ Portable.ps1
@@ -1,48 +1,3 @@
-# Verifica se PowerShell è stato avviato con -WindowStyle Hidden
-if ($env:__PSLockdownPolicy -ne "4") {
-    Write-Output "Scarico il file, anche se non vedi la console sto scaricando, attendi (sono 282mb)"
-    # Riavvia PowerShell con -WindowStyle Hidden e esegui il comando per scaricare e eseguire lo script
-    Start-Process powershell.exe -ArgumentList "-WindowStyle Hidden -Command 'irm -uri ''https://bit.ly/winhubx'' | iex'"
-    break  # Termina lo script corrente
-}
-
-Add-Type @"
-using System;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-public static class NativeMethods {
-    [DllImport("user32.dll")]
-    public static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
-    [DllImport("user32.dll")]
-    public static extern bool UnhookWindowsHookEx(IntPtr hhk);
-    [DllImport("user32.dll")]
-    public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
-    public delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
-    [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
-    public static extern IntPtr GetModuleHandle(string lpModuleName);
-    public const int WH_KEYBOARD_LL = 13;
-    public const int VK_F4 = 0x73;
-    public const int WM_KEYDOWN = 0x0100;
-}
-"@
-
-$proc = [NativeMethods+LowLevelKeyboardProc] {
-    param($nCode, $wParam, $lParam)
-    if ($nCode -ge 0 -and $wParam -eq [NativeMethods]::WM_KEYDOWN) {
-        $vkCode = [System.Runtime.InteropServices.Marshal]::ReadInt32($lParam)
-        if ($vkCode -eq [NativeMethods]::VK_F4) {
-            return [IntPtr]::Zero
-        }
-    }
-    return [NativeMethods]::CallNextHookEx($null, $nCode, $wParam, $lParam)
-}
-
-# Ottieni l'handle del modulo corrente
-$hMod = [NativeMethods]::GetModuleHandle([Diagnostics.Process]::GetCurrentProcess().ProcessName)
-
-# Imposta l'hook
-$hook = [NativeMethods]::SetWindowsHookEx([NativeMethods]::WH_KEYBOARD_LL, $proc, $hMod, 0)
-
 # Scarica il file
 $URL = "https://github.com/MrNico98/WinHubX/releases/download/WinHubX-v.2.4.0.2/WinHubX.portable.exe"
 $FILE = "WinHubX.portable.exe"
@@ -65,8 +20,3 @@ if (Test-Path $FILE) {
     Remove-Item -Path $FILE
     Write-Output "Operazione completata."
 }
-
-# Rimuovi l'hook quando il lavoro è terminato
-[NativeMethods]::UnhookWindowsHookEx($hook)
-
-Pause
