77282879ccabd66240c45319b6dd8b86cd10d2ce
Light Yagami <Maule2703@gmail.com>
2024-09-18 12:57:44 +0200

    Revert "Update last"
    This reverts commit 4f65cb077687fdd6d00cdc0d44c9f125bd162338.

--- WinHubX/Dialog/PacManDialog.Designer.cs
+++ WinHubX/Dialog/PacManDialog.Designer.cs
@@ -1,32 +1,65 @@
-﻿using System.ComponentModel;
-using System.Drawing;
-using System.Windows.Forms;
-
-namespace WinHubX.Dialog
+﻿namespace WinHubX.Dialog
 {
     partial class PacManDialog
     {
-        private IContainer components = null;
-        private Label lblPrompt;
-        private Button btnInstallPacMan;
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && components != null)
+            if (disposing && (components != null))
             {
                 components.Dispose();
             }
             base.Dispose(disposing);
         }
 
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
         private void InitializeComponent()
         {
-            components = new Container();
-            lblPrompt = CreatePromptLabel();
-            btnInstallPacMan = CreateInstallButton();
-
+            label1 = new Label();
+            btnInstallaPacMan = new Button();
             SuspendLayout();
-
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Italic, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Coral;
+            label1.Location = new Point(30, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(328, 20);
+            label1.TabIndex = 51;
+            label1.Text = "Scaricare PacMan per avviare gli apk esterni?";
+            label1.TextAlign = ContentAlignment.BottomCenter;
+            // 
+            // btnInstallaPacMan
+            // 
+            btnInstallaPacMan.Cursor = Cursors.Hand;
+            btnInstallaPacMan.FlatAppearance.BorderSize = 0;
+            btnInstallaPacMan.FlatStyle = FlatStyle.Flat;
+            btnInstallaPacMan.Font = new Font("Microsoft Sans Serif", 19.8000011F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnInstallaPacMan.ForeColor = Color.White;
+            btnInstallaPacMan.Location = new Point(54, 59);
+            btnInstallaPacMan.Margin = new Padding(3, 2, 3, 2);
+            btnInstallaPacMan.Name = "btnInstallaPacMan";
+            btnInstallaPacMan.Size = new Size(283, 73);
+            btnInstallaPacMan.TabIndex = 77;
+            btnInstallaPacMan.Text = "Installa PacMan";
+            btnInstallaPacMan.TextImageRelation = TextImageRelation.ImageBeforeText;
+            btnInstallaPacMan.UseVisualStyleBackColor = true;
+            btnInstallaPacMan.Click += btnInstallaPacMan_Click;
             // 
             // PacManDialog
             // 
@@ -34,8 +67,8 @@ namespace WinHubX.Dialog
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(37, 38, 39);
             ClientSize = new Size(422, 216);
-            Controls.Add(btnInstallPacMan);
-            Controls.Add(lblPrompt);
+            Controls.Add(btnInstallaPacMan);
+            Controls.Add(label1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "PacManDialog";
             Text = "PacMan";
@@ -43,40 +76,10 @@ namespace WinHubX.Dialog
             PerformLayout();
         }
 
-        private Label CreatePromptLabel()
-        {
-            return new Label
-            {
-                AutoSize = true,
-                Font = new Font("Microsoft Sans Serif", 12F, FontStyle.Italic, GraphicsUnit.Point, 0),
-                ForeColor = Color.Coral,
-                Location = new Point(30, 9),
-                Name = "lblPrompt",
-                Size = new Size(328, 20),
-                Text = "Scaricare PacMan per avviare gli apk esterni?",
-                TextAlign = ContentAlignment.BottomCenter
-            };
-        }
+        #endregion
 
-        private Button CreateInstallButton()
-        {
-            var button = new Button
-            {
-                Cursor = Cursors.Hand,
-                FlatAppearance = { BorderSize = 0 },
-                FlatStyle = FlatStyle.Flat,
-                Font = new Font("Microsoft Sans Serif", 19.8F, FontStyle.Bold, GraphicsUnit.Point, 0),
-                ForeColor = Color.White,
-                Location = new Point(54, 59),
-                Margin = new Padding(3, 2, 3, 2),
-                Name = "btnInstallPacMan",
-                Size = new Size(283, 73),
-                Text = "Installa PacMan",
-                TextImageRelation = TextImageRelation.ImageBeforeText,
-                UseVisualStyleBackColor = true
-            };
-            button.Click += BtnInstallPacMan_Click;
-            return button;
-        }
+        private Label label1;
+        private Button btnInstallaPacMan;
+        private Button btnClose;
     }
 }
